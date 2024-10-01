e000a449bd48066150099f97e8f08a0db1cce2a8
Nokuvimba <G00423951@atu.ie>
2024-10-01 10:43:03 +0100

    created lab1 for week 3.

---
+++ src/test/java/ie/atu/lab1week3/Lab1week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1week3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
