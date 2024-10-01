a37cd7116d569dd68cbc601c689fc9467c439fb1
Nokuvimba <G00423951@atu.ie>
2024-10-01 11:57:25 +0100

    created a calculate method for the assignment.

--- src/main/java/ie/atu/lab1week3/Week3.java
+++ src/main/java/ie/atu/lab1week3/Week3.java
@@ -1,9 +1,6 @@
 package ie.atu.lab1week3;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
@@ -26,5 +23,33 @@ public  class Week3 {
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "Hi " + name;
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int x, @RequestParam int y,@RequestParam String type){
+        int ans=0;
+
+        switch(type){
+            case "add":
+                ans= x+y;
+                break;
+            case "subtract":
+                ans= x-y;
+                break;
+            case "multiply":
+                ans= x*y;
+                break;
+            case "divide":
+                if (y==0){
+                 String error = "zero error";
+                 return error;
+                }
+                ans = x/y;
+                break;
+            default :
+                ans=0;
+        }
+
+        return "if you "+ type + " " + x + " and " + y + " the answer is " + ans;
     }
 }
