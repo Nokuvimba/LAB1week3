48ca7ad5462e238ad9985a324e5037faebdd8c9c
Nokuvimba <G00423951@atu.ie>
2024-10-01 11:14:39 +0100

    used @path variable to get the name.

--- src/main/java/ie/atu/lab1week3/Week3.java
+++ src/main/java/ie/atu/lab1week3/Week3.java
@@ -1,6 +1,7 @@
 package ie.atu.lab1week3;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,8 @@ public  class Week3 {
     public String hello(){
         return "hello";
     }
-
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hi " + name;
+    }
 }
